5631451ace2d1b9cee73d27df17fdd3f23e99db1
starplanter93 <starplanter93@gmail.com>
2023-09-11 20:19:48 +0900

    :white_check_mark: Useform register, handleSubmit 실험

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface InputForm {
+  id: string;
+}
 
 function App() {
+  const { register, handleSubmit } = useForm<InputForm>({ mode: 'onBlur' });
+
+  const onSubmit = (data: InputForm) => {
+    console.log(data, '제출성공');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('id')} />
+      </form>
     </div>
   );
 }
